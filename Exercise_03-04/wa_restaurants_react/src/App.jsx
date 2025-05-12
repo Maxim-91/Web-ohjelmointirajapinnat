eb501e22958aa729b1a0626c0acfb9253ccde4e8
Maxim <makc0791@gmail.com>
2025-05-12 18:55:18 +0200

    Exercise_03-04

---
+++ Exercise_03-04/wa_restaurants_react/src/App.jsx
@@ -0,0 +1,43 @@
+import RestaurantList from "./RestaurantList"
+import RatingsByRestaurant from "./RatingsByRestaurant";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Login from "./Login";
+import './App.css';
+import { useState } from "react";
+
+
+
+function App() {
+
+  
+
+  return (
+    
+    <Router>
+      <div>
+        {/* Navigation Bar (optional) */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Ratings</Link>
+            </li>
+            <li>
+              <Link to="/restaurants">Restaurants</Link>
+            </li>
+            
+          </ul>
+        </nav>
+
+       
+        <Routes>
+          
+          <Route path="/:id" element={<RatingsByRestaurant />} />
+          <Route path="/" element={<RestaurantList />} />
+          
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Exercise_03-04/wa_restaurants_react/src/RestaurantList.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import RestaurantCard from './RestaurantCard';
+import './RestaurantList.css'; // Import your CSS file
+
+function RestaurantList() {
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => 
+    {
+      fetch('http://localhost:8000/api/restaurants/ratings',
+           {headers: { accept: 'application/json' }})
+       .then(res => res.json())
+       .then(data => 
+          {
+            const formatted = data.map(item =>
+           ({
+             id: item.id,
+             imageUrl: '/images/cheese_burger.jpg',
+             name: item.name,
+             averageRating: item.rating ?? 0,
+             reviewCount: item.review_count,
+             cuisine: item.cuisine,
+             priceRange: item.price_range,
+             address: item.address
+            }));
+          setRestaurants(formatted);
+          })
+       .catch(console.error);
+    }, []);  
+    
+  return (
+    <div className="restaurant-list-container">
+      <div className="side-panel">
+        <h3>Actions</h3>
+        <button onClick={() => {}}>Add Rating</button>
+        {/* Add more action buttons here */}
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default RestaurantList;
